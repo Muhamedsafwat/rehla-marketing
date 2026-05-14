ebe9e112f9e33938dc5a93d77e37f7cf5e31f1d5
muhamed safwat <musafwat666@gmail.com>
2026-05-14 23:31:20 +0300

    Add google tag manager

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -98,10 +98,31 @@ export default async function RootLayout({
       dir={direction}
       className={`${plusJakartaSans.variable} ${almarai.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-56LRVVF6');`,
+          }}
+        />
+      </head>
       <body
         className={`min-h-full flex flex-col ${bodyFontClass}`}
         style={{ backgroundColor: "#0d0d0f" }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-56LRVVF6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           <Preloader />
           <BackgroundBeams />
